26326620d9496e4df3b2d66f2112e68041e4c63a
zhaochangchang <cczhao0113@163.com>
2024-06-06 11:21:11 +0800

    增加许可协议和README

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: './lib/main.ts',
-      name: 'cc-tools',
+      name: 'ctils',
       // fileName: (format) => `cc-tools.${format}.js` // 打包后的文件名
       fileName:`index` // 打包后的文件名
     },
